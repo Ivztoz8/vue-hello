a6a6dd9fd0d680c815655f7619deb929930de0c7
Ivan Toselli <ivan.toselli31@gmail.com>
2022-03-30 16:21:11 +0200

    Stp 0, 1: Exe + Bonus complete

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+var app = new Vue (
+    {
+        el: '#root',
+        data: {
+            message: 'Vue Hello World, dude!',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed congue accumsan nibh, non lobortis odio ultricies et. Aliquam erat volutpat. Sed non urna nec metus semper tincidunt. Nulla facilisi. Mauris scelerisque felis vel ipsum blandit, a viverra nisi cursus. Aliquam erat volutpat. Mauris elementum hendrerit fringilla. Nunc egestas velit sed ex malesuada, ac porttitor libero mollis. Nam porttitor massa id massa maximus, in molestie purus volutpat. Sed ac libero ac nibh convallis suscipit id vel mi. Aenean vestibulum, turpis ac ultrices lacinia, tortor leo rhoncus ex, eget accumsan dui turpis in lacus. Nullam sapien felis, ornare sit amet nisi ut, tempor auctor magna. Etiam sit amet molestie libero. Donec viverra cursus ante a venenatis. Nullam vitae tincidunt nibh, at facilisis purus.',
+            mieclassi: ['container','bg-primary','p-3'],
+            mieclassi1: ['text-center myc-colorblue'],
+            mieclassi2: ['text-center myc-colorgray'],
+            mieclassi3: ['rounded '],
+            urlrandoimg: 'https://picsum.photos/500/300',
+        }
+    }
+);
